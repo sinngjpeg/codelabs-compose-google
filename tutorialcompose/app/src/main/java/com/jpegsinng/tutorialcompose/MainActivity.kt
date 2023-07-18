d78867be1e7c554116a8fdaedf0b2d290b987718
ingrid <ingridsilvatec@outlook.com>
2023-07-18 19:53:35 -0300

    add preview function

--- tutorialcompose/app/src/main/java/com/jpegsinng/tutorialcompose/MainActivity.kt
+++ tutorialcompose/app/src/main/java/com/jpegsinng/tutorialcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +19,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MessageCard(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
 }
 
 
