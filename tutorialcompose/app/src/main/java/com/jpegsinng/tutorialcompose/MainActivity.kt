fed5b19cfa8d89f570b1069fa1680d183d6248b0
ingrid <ingridsilvatec@outlook.com>
2023-07-18 19:48:11 -0300

    add new composable function message card

--- tutorialcompose/app/src/main/java/com/jpegsinng/tutorialcompose/MainActivity.kt
+++ tutorialcompose/app/src/main/java/com/jpegsinng/tutorialcompose/MainActivity.kt
@@ -3,22 +3,22 @@ package com.jpegsinng.tutorialcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.jpegsinng.tutorialcompose.ui.theme.TutorialComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MessageCard(name = "Android")
         }
     }
 }
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
 
 
