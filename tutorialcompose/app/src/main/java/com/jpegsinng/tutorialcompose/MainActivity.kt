8d077df608593f889df78567bfec395d46d0e75d
ingrid <ingridsilvatec@outlook.com>
2023-07-18 21:20:41 -0300

    add color

--- tutorialcompose/app/src/main/java/com/jpegsinng/tutorialcompose/MainActivity.kt
+++ tutorialcompose/app/src/main/java/com/jpegsinng/tutorialcompose/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,12 +23,18 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jpegsinng.tutorialcompose.ui.theme.TutorialComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "JetPack Compose"))
+            TutorialComposeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "JetPack Compose"))
+
+                }
+            }
         }
     }
 }
@@ -40,11 +50,12 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = msg.author)
-            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.author, color = MaterialTheme.colorScheme.secondary)
+           Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }
@@ -53,9 +64,11 @@ fun MessageCard(msg: Message) {
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Colleague", "Hey, take a look at JetPack Compose, it's great!")
-    )
+    TutorialComposeTheme {
+        MessageCard(
+            msg = Message("Colleague", "Hey, take a look at JetPack Compose, it's great!")
+        )
+    }
 }
 
 
